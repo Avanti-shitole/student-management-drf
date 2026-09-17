92d0d5a6bf71cf836214941c153f6a5fc601e923
Avanti-shitole <avantishitole974@gmail.com>
2026-09-17 14:14:49 +0530

    Add user registration API and connect API URLs

--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import StudentListCreateAPIView, StudentDetailAPIView, LoginAPIView, LogoutAPIView
+from .views import StudentListCreateAPIView, StudentDetailAPIView, LoginAPIView, LogoutAPIView, RegisterAPIView
 
 urlpatterns = [
     path('students/', StudentListCreateAPIView.as_view()),
     path('students/<int:id>/', StudentDetailAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path("register/", RegisterAPIView.as_view(), name="register"),
 
 ]
--- students/views.py
+++ students/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 
@@ -47,6 +48,34 @@ class StudentDetailAPIView(APIView):
         student = Student.objects.get(id=id)
         student.delete()
         return Response({"message": "Student deleted successfully"})
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Username and password are required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {"error": "Username already exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        return Response(
+            {"message": "User registered successfully"},
+            status=status.HTTP_201_CREATED
+        )
     
 class LoginAPIView(APIView):
 
